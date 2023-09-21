b68b1e1323dbb3369255f5590aa7fd91d588499a
eladsez <eladsez36@gmail.com>
2023-09-21 17:32:02 +0300

    logger is donw with all level tests

--- STM32CubeIDE/Application/User/logger/logger.c
+++ STM32CubeIDE/Application/User/logger/logger.c
@@ -1,39 +1,123 @@
 
-
 #include "logger.h"
 
 
 extern lfs_t 						LFS;		/*!< LittleFS main instance */
 extern struct lfs_config 			LFS_CFG;	/*!< LittleFS configuration instance */
 extern FSI_Config                   DEFAULT_FSI_CONFIGURATION;
+lfs_file_t 							curr_log_file;		/*!< LittleFS file current log */
 
 
 void init_logger(void){
     /* The fsi init will config the littlefs with our flash and mount a rootfs for us */
     fsi_init(&DEFAULT_FSI_CONFIGURATION);
+
+    /* Create a log file for the current running */
+    lfs_file_open(&LFS, &curr_log_file, "/run.log", LFS_O_WRONLY | LFS_O_CREAT);
+}
+
+void get_level(log_level_t level, char* ret){
+    switch(level){
+        case LOG_INFO:
+            strcpy(ret, "INFO");
+            break;
+        case LOG_WARNING:
+            strcpy(ret, "WARNING");
+            break;
+        case LOG_ERROR:
+            strcpy(ret, "ERROR");
+            break;
+        case LOG_CRITICAL:
+            strcpy(ret, "CRITICAL");
+            break;
+        default:
+            strcpy(ret, "UNKNOWN");
+            break;
+    }
 }
 
 
-void logger_test(void){
-    /* Create a file */
-    lfs_file_t file;
-    lfs_file_open(&LFS, &file, "/run.log", LFS_O_WRONLY | LFS_O_CREAT);
+void level_log(log_level_t level, const char *message, ...) {
+    /* Get the current time with HAL_GetTick() */
+    uint32_t mstime = HAL_GetTick();
 
-    /* Write to the file */
-    const char message[] = "Hello World!\n";
-    lfs_file_write(&LFS, &file, message, sizeof(message)-1);
+    /* Make the time from integer ms to float seconds */
+    float seconds = (float)mstime / 1000.0f;
 
-    /* Close the file */
-    lfs_file_close(&LFS, &file);
+    /* Add the time to the message in the format: [seconds] level: message */
+    char log_message[MAX_LOG_MESSAGE_SIZE];
+    char level_str[10];
+    get_level(level, level_str);
 
-    /* Read the file */
-    char buffer[32];
-    lfs_file_open(&LFS, &file, "/run.log", LFS_O_RDONLY);
-    int size = lfs_file_read(&LFS, &file, buffer, sizeof(buffer));
-    lfs_file_close(&LFS, &file);
-    // print it with a for loop 
-    for(int i = 0; i < size; i++){
-        printf("%c", buffer[i]);
+    va_list args;
+    va_start(args, message);
+
+    int chars_written = snprintf(log_message, MAX_LOG_MESSAGE_SIZE, "[%f] %s: ", seconds, level_str);
+    if (chars_written < 0 || chars_written >= MAX_LOG_MESSAGE_SIZE) {
+        // Handle error or truncation
+        return;
     }
-    printf("\n");
+
+    chars_written = vsnprintf(log_message + chars_written, MAX_LOG_MESSAGE_SIZE - chars_written, message, args);
+
+    va_end(args);
+
+    lfs_file_write(&LFS, &curr_log_file, log_message, strlen(log_message));
+}
+
+void log_info(const char *message, ...) {
+    va_list args;
+    va_start(args, message);
+    level_log(LOG_INFO, message, args);
+    va_end(args);
+}
+
+void log_warning(const char *message, ...) {
+    va_list args;
+    va_start(args, message);
+    level_log(LOG_WARNING, message, args);
+    va_end(args);
+}
+
+void log_error(const char *message, ...) {
+    va_list args;
+    va_start(args, message);
+    level_log(LOG_ERROR, message, args);
+    va_end(args);
+}
+
+void log_critical(const char *message, ...) {
+    va_list args;
+    va_start(args, message);
+    level_log(LOG_CRITICAL, message, args);
+    va_end(args);
+}
+
+void close_logger(void){
+    /* Close the file */
+    lfs_file_close(&LFS, &curr_log_file);
+}
+
+int read_log(char* buff, uint32_t size){
+    /* Read the file */
+    lfs_file_open(&LFS, &curr_log_file, "/run.log", LFS_O_RDONLY);
+    int size_read = lfs_file_read(&LFS, &curr_log_file, buff, size);
+    lfs_file_close(&LFS, &curr_log_file);
+    buff[size_read] = '\0';
+    return size_read;
+}
+
+void logger_test(void){
+    /* Test the logger */
+    init_logger();
+    log_info("This is a test info message");
+    log_warning("This is a test warning message");
+    log_error("This is a test error message");
+    log_critical("This is a test critical message");
+    close_logger();
+
+    /* Read the log file */
+    char buff[1000] = {0};
+    read_log(buff, 1000);
+    printf("%s", buff);
 }
--- STM32CubeIDE/Application/User/logger/logger.h
+++ STM32CubeIDE/Application/User/logger/logger.h
@@ -6,11 +6,34 @@
 #include "FSI/LittleFS/lfs.h"
 #include "FSI/fsi.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
 
+#define MAX_LOG_MESSAGE_SIZE 256
+
+
+typedef enum {
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR,
+    LOG_CRITICAL
+} log_level_t;
 
 
 void init_logger(void);
 
 void logger_test(void);
+
+void log_info(const char *message, ...);
+
+void log_warning(const char *message, ...);
+
+void log_error(const char *message, ...);
+
+void log_critical(const char *message, ...);
+
+void close_logger(void);
+
+int read_log(char* buff, uint32_t size);
 
 #endif /* LOGGER_LOGGER_H */
